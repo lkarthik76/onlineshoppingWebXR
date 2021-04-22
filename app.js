4db384da8a70e2a6b504b0c298367b29efbe3329
LK <karthikeyan-l@hcl.com>
2021-04-22 16:18:07 +0530

    changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import * as THREE from './libs/three.module.js';
 import { GLTFLoader } from './libs/GLTFLoader.js';
 import { RGBELoader } from './libs/RGBELoader.js';
-import { ARButton } from './libs/ARButton.js';
+import { ARButton } from './libs/webxr/ARButton.js';
 import { LoadingBar } from './libs/LoadingBar.js';
 
 class App{
